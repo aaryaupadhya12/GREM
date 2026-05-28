281d917fb0d1400ad06929fafbec1e08c1073c4f
Aarya Upadhya <aarya.upadhya20@gmail.com>
2026-05-28 16:00:26 +0530

    MongoDB advances added end to end working

--- Baseline_Test/api_testing.py
+++ Baseline_Test/api_testing.py
@@ -6,3 +6,11 @@ load_dotenv()
 api_key = os.getenv("GROQ_API_KEY_AGG")
 
 print(api_key)
+
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client = MongoClient(os.environ.get("MONGO_URI"))
+print(client.list_database_names())
